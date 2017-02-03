c51534941c2b0c94a5122ea717a1a4ffad920336
evanglist <evanglist@Evanglist>
2017-02-03 23:39:24 +0800

    加解密异常报错规范化； 添加了信息解密方法。

--- src/cn/clvstudio/tool/Crypto.java
+++ src/cn/clvstudio/tool/Crypto.java
@@ -1,9 +1,15 @@
 package cn.clvstudio.tool;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.apaches.commons.codec.binary.Base64;
@@ -22,7 +28,7 @@ public class Crypto {
 	 * @return
 	 * @throws Exception
 	 */
-    public String Encrypt(String sSrc, String sKey) throws Exception {
+    public String encrypt(String sSrc, String sKey) {
         if (sKey == null) {
             System.out.print("Key值为空");
             return null;
@@ -31,15 +37,38 @@ public class Crypto {
             System.out.print("Key不是16的倍数");
             return null;
         }
-        byte[] raw = sKey.getBytes("utf-8");
-        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+		try {
+			byte[] raw = sKey.getBytes("utf-8");
+			SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+		
+        
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
         byte[] encrypted = cipher.doFinal(sSrc.getBytes("utf-8"));
 
         return new Base64().encodeToString(encrypted);
+        } catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		}
+		return null;
     }
-
+    public String encryptMessage(String message,String phone,String securityKey){
+    	String encrypt = encrypt(message,builderMessage(phone,securityKey));
+    	if(encrypt != null ){
+    		return encrypt;
+    	}
+    	return "fail";
+    }
     /**
      * AES解密
      * @param sSrc
@@ -47,7 +76,7 @@ public class Crypto {
      * @return
      * @throws Exception
      */
-    public String Decrypt(String sSrc, String sKey) throws Exception {
+    public String decrypt(String sSrc, String sKey) {
 //    	System.out.println("AES...: "+sSrc);
     	try {
             // 判断Key是否正确
@@ -61,35 +90,46 @@ public class Crypto {
                 return null;
             }
             byte[] raw = sKey.getBytes("utf-8");
+			
             SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.DECRYPT_MODE, skeySpec);
             byte[] encrypted1 = new Base64().decode(sSrc);//先用base64解密
-            try {
-                byte[] original = cipher.doFinal(encrypted1);
-                String originalString = new String(original,"utf-8");
-                return originalString;
-            } catch (Exception e) {
-                System.out.println(e.toString());
-                return null;
-            }
-        } catch (Exception ex) {
-            System.out.println(ex.toString());
-            return null;
-        }
+            byte[] original = cipher.doFinal(encrypted1);
+            String originalString = new String(original,"utf-8");
+            return originalString;
+    	} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+				return null;
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+			return null;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+			return null;
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+			return null;
+		}
     }
     /**
      * 解密经过AES"时间钥匙"加密的信息
      * @param enId
      * @return
      */
-    public String DecryptTime(String message){
+    public String decryptTime(String message){
     	long time = System.currentTimeMillis();
 		StringBuilder IdKeysb = new StringBuilder(getMD5(getMD5(Long.valueOf(time/100000*100000).toString())));
 		
 		String IdKey = IdKeysb.substring(0, 16);
     	try {
-    		return Decrypt(message,IdKey);
+    		return decrypt(message,IdKey);
 		} catch (Exception e) {
 			System.out.println("解密身份号失败");
 			e.printStackTrace();
@@ -102,8 +142,22 @@ public class Crypto {
      * @param enMessage
      * @return
      */
-    public String DecrypMessage(String enMessage,String phone,String security_key){
-    	StringBuilder sb = new StringBuilder(security_key);
+    public String decrypMessage(String message,String phone,String securityKey){
+		String decrypt =  decrypt(message,builderMessage(phone,securityKey));
+		if(decrypt != null){
+			return decrypt;
+		}
+    	return "fail";
+    }
+    /**
+     * 生成信息匙
+     * @param enMessage
+     * @param phone
+     * @param securityKey
+     * @return
+     */
+    public String builderMessage(String phone,String securityKey){
+    	StringBuilder sb = new StringBuilder(securityKey);
 		sb.delete(0, 16);
 		
 		String str = sb.toString();
@@ -123,15 +177,7 @@ public class Crypto {
 			Num /= 10;
 		}
 		sb.append(Integer.valueOf(count).toString());
-		String messageKey = sb.toString();
-//		System.out.println(messageKey);
-    	try {
-			return Decrypt(enMessage,messageKey);
-		} catch (Exception e) {
-			System.out.println("解密信息失败");
-			e.printStackTrace();
-		}
-    	return "fail";
+		return sb.toString();
     }
 	public static long sum(long num){
 		return num<10?num:num%10+sum(num/10);
@@ -142,17 +188,18 @@ public class Crypto {
      * @return
      */
     public static String getMD5(String str) {
-	    try {
-	        // 生成一个MD5加密计算摘要
-	        MessageDigest md = MessageDigest.getInstance("MD5");
-	        // 计算md5函数
-	        md.update(str.getBytes());
-	        // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
-	        // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
-	        return new BigInteger(1, md.digest()).toString(16);
-	    } catch (Exception e) {
-	        System.out.println("MD5加密出错");
-	    }
-		return null;
+        // 生成一个MD5加密计算摘要
+        MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
+        // 计算md5函数
+        md.update(str.getBytes());
+        // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
+        // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
+        return new BigInteger(1, md.digest()).toString(16);
 	}
 }
