5b73f62c6695c921c45a206e0d42988130a0d28f
evanglist <evanglist@Evanglist>
2017-02-04 00:02:44 +0800

    页面视图添加及修改

--- src/com/clv/controller/ResumeController.java
+++ src/com/clv/controller/ResumeController.java
@@ -6,32 +6,69 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ResumeController {
-	@RequestMapping(value="/user/resume/addPhoto",method = RequestMethod.POST)
+	//********************************
+	//**		简历-->风采			**
+	//********************************
+	@RequestMapping(value="/user/resume/photo/addPhoto",method = RequestMethod.POST)
 	public String addPhoto(){
 		return "addPhoto";
 	}
-	@RequestMapping(value="/user/resume/deletePhoto",method = RequestMethod.POST)
+	@RequestMapping(value="/user/resume/photo/deletePhoto",method = RequestMethod.POST)
 	public String deletePhoto(){
 		return "deletePhoto";
 	}
-	@RequestMapping(value="/user/resume/selectPhoto",method = RequestMethod.POST)
+	@RequestMapping(value="/user/resume/photo/selectPhoto",method = RequestMethod.POST)
 	public String selectPhoto(){
 		return "selectPhoto";
 	}
-	@RequestMapping(value="/user/resume/addSkill",method = RequestMethod.POST)
+	//********************************
+	//**		简历-->特长			**
+	//********************************
+	@RequestMapping(value="/user/resume/skill/addSkill",method = RequestMethod.POST)
 	public String addSkill(){
 		return "addSkill";
 	}
-	@RequestMapping(value="/user/resume/selectSkill",method = RequestMethod.POST)
+	@RequestMapping(value="/user/resume/skill/selectSkill",method = RequestMethod.POST)
 	public String selectSkill(){
 		return "selectSkill";
 	}
-	@RequestMapping(value="/user/resume/deleteSkill",method = RequestMethod.POST)
+	@RequestMapping(value="/user/resume/skill/deleteSkill",method = RequestMethod.POST)
 	public String deleteSkill(){
 		return "deleteSkill";
 	}
-	@RequestMapping(value="/user/resume/modifySkill",method = RequestMethod.POST)
+	@RequestMapping(value="/user/resume/skill/modifySkill",method = RequestMethod.POST)
 	public String modifySkill(){
 		return "modifySkill";
+	}
+	//********************************
+	//**	用户提交身份认证相关		**
+	//********************************
+	@RequestMapping(value="/user/resume/identity/identityAuthentication",method = RequestMethod.POST)
+	public String identityAuthentication(){
+		return "identityAuthentication";
+	}
+	@RequestMapping(value="/user/resume/identity/modifyIdentityAuthentication",method = RequestMethod.POST)
+	public String modifyIdentityAuthentication(){
+		return "modifyIdentityAuthentication";
+	}
+	@RequestMapping(value="/user/resume/identity/uploadCertificatePhoto",method = RequestMethod.POST)
+	public String uploadCertificatePhoto(){
+		return "uploadCertificatePhoto";
+	}
+	@RequestMapping(value="/user/resume/identity/selectIdentityAuditConclusion",method = RequestMethod.POST)
+	public String selectIdentityAuditConclusion(){
+		return "selectIdentityAuditConclusion";
+	}
+	@RequestMapping(value="/user/resume/identity/modifyHeight",method = RequestMethod.POST)
+	public String modifyHeight(){
+		return "modifyHeight";
+	}
+	@RequestMapping(value="/user/resume/identity/modifyEmail",method = RequestMethod.POST)
+	public String modifyEmail(){
+		return "modifyEmail";
+	}
+	@RequestMapping(value="/user/resume/identity/getInformation",method = RequestMethod.POST)
+	public String getInformation(){
+		return "getInformation";
 	}
 }
