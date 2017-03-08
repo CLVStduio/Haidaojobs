9de014c704f5eebfe411db4a0feac6ca99c05493
evanglist <evanglist@activate.adobe.com>
2017-03-08 16:23:50 +0800

    修改注释

--- src/cn/clvstudio/tool/BuilderKey.java
+++ src/cn/clvstudio/tool/BuilderKey.java
@@ -15,38 +15,48 @@ public class BuilderKey {
 	 * 		:手机号
 	 * @return
 	 */
-		public String builderSecurityKey(String phone){
-			StringBuilder sb = new StringBuilder(getMD5(phone));
-			sb.delete(0,16);
-			for(int i = 0 ;i<13;i++){
-				char letter = (char)(Math.random()*26+65);
-				sb.append(letter);
-			}
-			String security_key = sb.toString();
-			return security_key;
+	public String builderSecurityKey(String phone){
+		StringBuilder sb = new StringBuilder(getMD5(phone));
+		sb.delete(0,16);
+		for(int i = 0 ;i<13;i++){
+			char letter = (char)(Math.random()*26+65);
+			sb.append(letter);
 		}
-		
-		public String builderComplementKey(String phone){
-			StringBuilder sb = new StringBuilder(getMD5(phone));
-			sb.delete(16,32);
-			long time = System.currentTimeMillis();
-			sb.append(Long.valueOf(time).toString());
-			String complemen_key = sb.toString();
-			return complemen_key;
-		}
-		
-		public String getMD5(String str) {
-		    try {
-		        // 生成一个MD5加密计算摘要
-		        MessageDigest md = MessageDigest.getInstance("MD5");
-		        // 计算md5函数
-		        md.update(str.getBytes());
-		        // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
-		        // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
-		        return new BigInteger(1, md.digest()).toString(16);
-		    } catch (Exception e) {
-		        System.out.println("MD5加密出错");
-		    }
-			return null;
-		}
+		String security_key = sb.toString();
+		return security_key;
+	}
+	
+	/**
+	 * 生成对应匙
+	 * @param phone
+	 * @return
+	 */
+	public String builderComplementKey(String phone){
+		StringBuilder sb = new StringBuilder(getMD5(phone));
+		sb.delete(16,32);
+		long time = System.currentTimeMillis();
+		sb.append(Long.valueOf(time).toString());
+		String complemen_key = sb.toString();
+		return complemen_key;
+	}
+	
+	/**
+	 * md5加密
+	 * @param str
+	 * @return
+	 */
+	public String getMD5(String str) {
+	    try {
+	        // 生成一个MD5加密计算摘要
+	        MessageDigest md = MessageDigest.getInstance("MD5");
+	        // 计算md5函数
+	        md.update(str.getBytes());
+	        // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
+	        // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
+	        return new BigInteger(1, md.digest()).toString(16);
+	    } catch (Exception e) {
+//	        System.out.println("MD5加密出错");
+	    }
+		return null;
+	}
 }
