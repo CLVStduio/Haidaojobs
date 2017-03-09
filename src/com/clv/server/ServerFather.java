ba022dcef137cfab34560b474c4f4de419ef8180
evanglist <evanglist@Evanglist>
2017-03-09 15:01:40 +0800

    将常用字符，进行常量化

---
+++ src/com/clv/server/ServerFather.java
@@ -0,0 +1,15 @@
+package com.clv.server;
+
+public interface ServerFather {
+	//用户
+	static final String USERPASSWORD = "userPassword";
+	static final String REGISTEREDATE = "registeredDate";
+	static final String COMPLEMENTKEY = "complementKey";
+	static final String SECURITYKEY = "securityKey";
+	static final String USERID = "userId";
+	static final String PHONENO = "phoneNo";
+	static final String HEADPORTRAITNAME = "headPortraitName";
+	//返回信息
+	static final String FAIL = "fail";
+	static final String SUCCESS = "success";
+}
